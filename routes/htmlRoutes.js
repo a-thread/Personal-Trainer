48d745811323a263a0791b778c58c1a5735327e1
a-thread <aiden.threadgoode@gmail.com>
2020-10-16 23:20:54 -0400

    correcting paths

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -5,11 +5,11 @@ router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
-router.get("/workouts", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/excercise.html"))
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 });
 
-router.get("/range", (req, res) => {
+router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
